13ce95263171f869acd47fe3521d19fc4f4fb0d9
Conrad Lovegrove <conrad.lovegrove@gmail.com>
2023-04-18 14:17:18 +0100

    main

---
+++ Main.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+# Load the data using np.load
+data = np.load('resources/training_images_full.npz', allow_pickle=True)
+
+# Extract the images
+images = data['images']
+# and the data points
+pts = data['points']
+
+print(images.shape, pts.shape)
+
+# Load the data that only has a subset of annotations using np.load
+data = np.load('resources/training_images_subset.npz', allow_pickle=True)
+
+# Extract the images
+images_subset = data['images']
+# and the data points
+pts_subset = data['points']
+
+print(images_subset.shape, pts_subset.shape)
+
+test_data = np.load('resources/test_images.npz', allow_pickle=True)
+test_images = test_data['images']
+print(test_images.shape)
+
+example_data = np.load('resources/examples.npz', allow_pickle=True)
+example_images = example_data['images']
+print(example_images.shape)
+
+#visualises points data
+def visualise_pts(img, pts):
+  import matplotlib.pyplot as plt
+  plt.imshow(img)
+  plt.plot(pts[:, 0], pts[:, 1], '+r', ms=7)
+  plt.show()
+
+#testing visualisation
+'''
+for i in range(3):
+  idx = np.random.randint(0, images.shape[0])
+  visualise_pts(images[idx, ...], pts[idx, ...])
+
+for i in range(3):
+  idx = np.random.randint(0, images.shape[0])
+  visualise_pts(images_subset[idx, ...], pts_subset[idx, ...])
+  '''
+
+#extracts subset of points from full set
+def extract_subset_of_points(pts):
+  indices = (20, 29, 16, 32, 38)
+  if len(pts.shape) == 3:
+    return pts[:, indices, :]
+  elif len(pts.shape) == 2:
+    return pts[indices, :]
+  
+  #test above
+'''
+for i in range(3):
+  idx = np.random.randint(0, images.shape[0])
+  visualise_pts(images[idx, ...], extract_subset_of_points(pts[idx, ...]))
+  '''
+
+#calculate euclidean distance (incomplete)
+def euclid_dist(pred_pts, gt_pts):
+  """
+  Calculate the euclidean distance between pairs of points
+  :param pred_pts: The predicted points
+  :param gt_pts: The ground truth points
+  :return: An array of shape (no_points,) containing the distance of each predicted point from the ground truth
+  """
+  pred_pts = np.reshape(pred_pts, (-1, 2))
+  gt_pts = np.reshape(gt_pts, (-1, 2))
+  return np.sqrt(np.sum(np.square(pred_pts - gt_pts), axis=-1))
+
+#save as csv [for when finished :)))))))]
+def save_as_csv(points, location = '.'):
+    """
+    Save the points out as a .csv file
+    :param points: numpy array of shape (no_test_images, no_points, 2) to be saved
+    :param location: Directory to save results.csv in. Default to current working directory
+    """
+    assert points.shape[0]==554, 'wrong number of image points, should be 554 test images'
+    assert np.prod(points.shape[1:])==44*2, 'wrong number of points provided. There should be 34 points with 2 values (x,y) per point'
+    np.savetxt(location + '/results.csv', np.reshape(points, (points.shape[0], -1)), delimiter=',')
